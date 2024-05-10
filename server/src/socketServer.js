c28e03eb60bb3450d450143835257f549c211361
seniorpung <pungsurich@proton.me>
2024-05-10 14:15:52 +0700

    Init Project

---
+++ server/src/socketServer.js
@@ -0,0 +1,126 @@
+const { Server } = require("socket.io");
+const { v4: uuid } = require("uuid");
+const { getOpenai } = require("./ai");
+
+let sessions = {};
+
+const registerSocketServer = (server) => {
+  const io = new Server(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log(`user connected ${socket.id}`);
+
+    socket.on("session-history", (data) => {
+      sessionHistoryHandler(socket, data);
+    });
+
+    socket.on("conversation-message", (data) => {
+      conversationMessageHandler(socket, data);
+    });
+
+    socket.on("conversation-delete", (data) => {
+      conversationDeleteHandler(socket, data);
+    });
+  });
+};
+
+const sessionHistoryHandler = (socket, data) => {
+  const { sessionId } = data;
+
+  if (sessions[sessionId]) {
+    // send existing session data back to user
+
+    socket.emit("session-details", {
+      sessionId,
+      conversations: sessions[sessionId],
+    });
+  } else {
+    const newSessionId = uuid();
+
+    sessions[newSessionId] = [];
+
+    const sessionDetails = {
+      sessionId: newSessionId,
+      conversations: [],
+    };
+
+    socket.emit("session-details", sessionDetails);
+  }
+};
+
+const conversationMessageHandler = async (socket, data) => {
+  const { sessionId, message, conversationId } = data;
+
+  const openai = getOpenai();
+
+  const previousConversationMessages = [];
+
+  if (sessions[sessionId]) {
+    const existingConversation = sessions[sessionId].find(
+      (c) => c.id === conversationId
+    );
+
+    if (existingConversation) {
+      previousConversationMessages.push(
+        ...existingConversation.messages.map((m) => ({
+          content: m.content,
+          role: m.aiMessage ? "assistant" : "user",
+        }))
+      );
+    }
+
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        ...previousConversationMessages,
+        { role: "user", content: message.content },
+      ],
+    });
+
+    const aiMessageContent = response?.data?.choices[0]?.message?.content;
+
+    const aiMessage = {
+      content: aiMessageContent
+        ? aiMessageContent
+        : "Error occurred when trying to get message from the AI",
+      id: uuid(),
+      aiMessage: true,
+    };
+
+    const conversation = sessions[sessionId].find(
+      (c) => c.id === conversationId
+    );
+
+    if (!conversation) {
+      sessions[sessionId].push({
+        id: conversationId,
+        messages: [message, aiMessage],
+      });
+    }
+
+    if (conversation) {
+      conversation.messages.push(message, aiMessage);
+    }
+
+    const updatedConversation = sessions[sessionId].find(
+      (c) => c.id === conversationId
+    );
+
+    socket.emit("conversation-details", updatedConversation);
+  }
+};
+
+const conversationDeleteHandler = (_, data) => {
+  const { sessionId } = data;
+
+  if (sessions[sessionId]) {
+    sessions[sessionId] = [];
+  }
+};
+
+module.exports = { registerSocketServer };
